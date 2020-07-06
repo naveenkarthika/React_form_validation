bad2282acb50c72415c2227ea46e8cad24b05e6a
naveenkarthika <yuvannaveen65@gmail.com>
2020-07-06 15:23:22 -0500

    My First React Form_validation site added in git repository

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Link,NavLink} from "react-router-dom";
+import FormValidation from "./Forms/FormValidation";
+import './App.css';
+
+
+
+function App() {
+  return (
+    
+        <div className="App">
+          <FormValidation/>
+        </div>
+
+  );
+}
+
+export default App;
