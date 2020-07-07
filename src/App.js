82e3329ebfd1972a5c638125dc4d3e5d81d304c3
naveenkarthika <yuvannaveen65@gmail.com>
2020-07-07 06:54:00 -0500

    Newly add more components & Routing will configure this commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import React from 'react';
 import FormValidation from "./Forms/FormValidation";
 import './App.css';
+import Home from './Home/Home';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Login from './Forms/Login';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    
-        <div className="App">
-          <FormValidation/>
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/" id="link">Home</Link>
+              </li>
+              <li>
+                <Link to="/about" id="link">About</Link>
+              </li>
+              <li>
+                <Link to="/contact"id="link">Contact</Link>
+              </li> 
+              <li>
+                <Link to="/register" id="link">Register</Link>
+              </li>
+              <li>
+                <Link to="/login" id="link">Login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/register">
+              <FormValidation />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
+      </Router>
+    </div>
 
   );
 }
--- src/Forms/Login.js
+++ src/Forms/Login.js
@@ -5,40 +5,25 @@ import * as Yup from 'yup';
 const FormValidation = () => {
     const formik=useFormik({
         initialValues:{
-            name:"",
             email:"",
-            password: "",
-            confirmPassword:""
-
+            password: ""
         },
         onSubmit:(userInput) => {
             console.log(userInput);
         },
         validationSchema:Yup.object({
-            name:Yup.string()
-            .required("This field is required")
-            .min(5,"Name must be 5 character")
-            .max(15,"Name should be below 15 character"),
             email:Yup.string()
             .email()
             .required("This field is required"),
             password:Yup.string()
             .required("This field is required"),
-            confirmPassword:Yup.string()
-            .required("This field is required")
-            .oneOf([Yup.ref("password"),null],"confirmPassword & password must be same")
         })
     })
 
     return(
         <div className="container">
-            <h2>Create your new Account?</h2>
+            <h2>User Login</h2>
             <form autoComplete="off" onSubmit={formik.handleSubmit}>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input type="text"className="form-control" name="name" onChange={formik.handleChange} value={formik.values.name}/>
-                    {formik.errors.name ? <div className="text-danger">{formik.errors.name}</div> : null}
-                </div>
                 <div className="form-group">
                     <label>Email</label>
                     <input type="text"className="form-control" name="email" onChange={formik.handleChange} value={formik.values.email} />
@@ -49,12 +34,7 @@ const FormValidation = () => {
                     <input type="text"className="form-control" name="password" onChange={formik.handleChange} value={formik.values.password} />
                     {formik.errors.password ? <div className="text-danger">{formik.errors.password}</div> : null}
                 </div>
-                <div className="form-group">
-                    <label>Confirm Password</label>
-                    <input type="text"className="form-control" name="confirmPassword" onChange={formik.handleChange} value={formik.values.confirmPassword} />
-                    {formik.errors.confirmPassword ? <div className="text-danger">{formik.errors.confirmPassword}</div> : null}
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Login</button>
             </form>
         </div>
     );
